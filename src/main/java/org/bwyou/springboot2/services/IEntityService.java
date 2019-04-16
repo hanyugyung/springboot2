db5b9a0e2c93b9b8c344c0f071bbe17dd8655eca
yousky <y1o2u3@gmail.com>
2019-04-16 09:54:44 +0900

    IEntityService와 MetaData, PageResultViewModel 추가.

---
+++ src/main/java/org/bwyou/springboot2/services/IEntityService.java
@@ -0,0 +1,23 @@
+package org.bwyou.springboot2.services;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.validation.BindingResult;
+
+public interface IEntityService<TEntity, TId> {
+	List<TEntity> GetList();
+	List<TEntity> GetList(String sort);
+	Page<TEntity> GetList(String sort, int pageNumber, int pageSize);
+	List<TEntity> GetFilteredList(Specification<TEntity> spec);
+	List<TEntity> GetFilteredList(Specification<TEntity> spec, String sort);
+	Page<TEntity> GetFilteredList(Specification<TEntity> spec, String sort, int pageNumber, int pageSize);
+	
+    TEntity Get(int id);
+    TEntity Get(Specification<TEntity> spec);
+    TEntity ValidAndCreate(TEntity entity, BindingResult bindingResult);
+    List<TEntity> ValidAndCreate(List<TEntity> entities, BindingResult bindingResult);
+    TEntity ValidAndUpdate(int id, TEntity entity, BindingResult bindingResult);
+    int ValidAndDelete(int id, BindingResult bindingResult);
+}
